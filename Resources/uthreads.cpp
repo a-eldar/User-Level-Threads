53ad5dbc008ba299e225eaac5e233fbc19a21424
Eldar Amar <eldar.amar@mail.huji.ac.il>
2024-06-05 09:53:29 +0300

    Added uthreads.cpp

---
+++ Resources/uthreads.cpp
@@ -0,0 +1,17 @@
+//
+// Created by amare on 6/5/2024.
+//
+
+/**
+ * @brief Represents a single thread.
+ */
+class Thread {
+
+};
+
+/**
+* @brief Data structure for holding all existing threads.
+*/
+class ThreadSet {
+
+};
