729d6c635813d3ae6e4871d8ea36e7375e7b3f92
Eldar Amar <eldar.amar@mail.huji.ac.il>
2024-06-05 21:14:55 +0300

    Spaced things out

--- Resources/uthreads.cpp
+++ Resources/uthreads.cpp
@@ -2,12 +2,16 @@
 // Created by amare on 6/5/2024.
 //
 
-#include <set>
+#include <vector>
 #include <queue>
+#include "uthreads.h"
 
 enum ThreadState {
     READY, RUNNING, BLOCKED
 };
+
+
+
 
 /**
  * @brief Represents a single thread.
@@ -22,13 +26,33 @@ public: explicit Thread(int tid) {
     }
 };
 
+
+
+
 class UThreadManager {
-private: std::set<Thread> thread_set;
+private: std::vector<Thread> threads;
 private: std::queue<Thread> thread_queue;
 
+public: UThreadManager() {
+    // Preallocate MAX_THREADS_NUM spaces for threads
+    threads.reserve(MAX_THREAD_NUM);
+}
 
+/** @brief Adds a new thread.
+ * @return tid */
+public: int SpawnThread() {
+    // Implement
+}
 };
+
+
+
+
+
 
 UThreadManager manager; // Global variable responsible for controlling all of the threads.
 // all library functions will use this global object operate on the threads.
 // Note: uthread_init returns -1 upon receiving a negative value.
+
+
+/* API functions implementation: */
