b43efdf55d7f6eefd58506d145d8ac8082bcd8b0
Eldar Amar <eldar.amar@mail.huji.ac.il>
2024-06-05 22:29:45 +0300

    start of basic architecture

--- Resources/uthreads.cpp
+++ Resources/uthreads.cpp
@@ -6,6 +6,8 @@
 #include <queue>
 #include "uthreads.h"
 
+typedef int micro_sec;
+
 enum ThreadState {
     READY, RUNNING, BLOCKED
 };
@@ -17,32 +19,75 @@ enum ThreadState {
  * @brief Represents a single thread.
  */
 class Thread {
-private: ThreadState state;
-private: int tid;
+    ThreadState state;
+    int tid;
 
 public: explicit Thread(int tid) {
         this->tid = tid;
         this->state = READY;
+    }
+
+    ThreadState getState() const {
+        return state;
+    }
+
+    int getTid() const {
+        return tid;
+    }
+
+    void setState(ThreadState new_state) {
+        state = new_state;
     }
 };
 
 
 
 
+
+/**
+ * @brief Class instance manages all UThreads operations
+ *      and thread holding.
+ */
 class UThreadManager {
-private: std::vector<Thread> threads;
-private: std::queue<Thread> thread_queue;
+    std::vector<Thread*> threads;
+    std::queue<Thread*> thread_queue;
+    micro_sec quantum;
+    int num_threads;
 
 public: UThreadManager() {
-    // Preallocate MAX_THREADS_NUM spaces for threads
-    threads.reserve(MAX_THREAD_NUM);
+    threads.reserve(MAX_THREAD_NUM); // Preallocate MAX_THREADS_NUM spaces for threads
+    std::fill(threads.begin(), threads.end(), nullptr);
+    num_threads = 0;
 }
 
-/** @brief Adds a new thread.
- * @return tid */
-public: int SpawnThread() {
-    // Implement
-}
+    /** @brief Initialize UThreads lib (no validity check) */
+    void init(micro_sec quantum) {
+        this->quantum = quantum;
+        // Perhaps this->quantum is useless.
+        // Perhaps can be used once in the lib function.
+
+        threads[0] = new Thread(0);
+        threads[0]->setState(RUNNING);
+        num_threads++;
+    }
+
+    /**
+     * @brief Adds a new thread.
+     * @return tid
+     */
+    int spawnThread() {
+        num_threads++;
+        // TODO: Implement
+
+        // TODO: If we need to use thread_queue, ensure to add thread to queue \
+            every time we set its state to READY!
+    }
+
+    ~UThreadManager() {
+        for (const auto& thread_ptr : threads) {
+            delete thread_ptr;
+        }
+    }
 };
 
 
