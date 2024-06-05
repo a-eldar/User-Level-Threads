20a8a4fe5d2f6e3a34046e608edb044f0523a0b5
Eldar Amar <eldar.amar@mail.huji.ac.il>
2024-06-05 13:54:40 +0300

    Added classes

--- Resources/uthreads.cpp
+++ Resources/uthreads.cpp
@@ -2,7 +2,10 @@
 // Created by amare on 6/5/2024.
 //
 
-enum THREAD_STATES {
+#include <set>
+#include <queue>
+
+enum ThreadState {
     READY, RUNNING, BLOCKED
 };
 
@@ -10,12 +13,22 @@ enum THREAD_STATES {
  * @brief Represents a single thread.
  */
 class Thread {
+private: ThreadState state;
+private: int tid;
+
+public: explicit Thread(int tid) {
+        this->tid = tid;
+        this->state = READY;
+    }
+};
+
+class UThreadManager {
+private: std::set<Thread> thread_set;
+private: std::queue<Thread> thread_queue;
+
 
 };
 
-/**
-* @brief Data structure for holding all existing threads.
-*/
-class ThreadSet {
-
-};
+UThreadManager manager; // Global variable responsible for controlling all of the threads.
+// all library functions will use this global object operate on the threads.
+// Note: uthread_init returns -1 upon receiving a negative value.
